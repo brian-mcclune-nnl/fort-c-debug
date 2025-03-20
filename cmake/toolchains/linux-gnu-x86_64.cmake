9aa3442255942dfeb7cb1994485ecc2b3bdd227d
Brian McClune <brian.mcclune@unnpp.gov>
2025-03-20 11:57:15 +0000

    docs: explain CMake compiler flags in toolchain file
    - Add detailed comments about CMAKE_<LANG>_FLAGS_INIT vs CMAKE_<LANG>_FLAGS
    - Document how different types of flags combine
    - Include examples of flag inheritance and precedence
    - Demonstrate proper usage in toolchain files

--- cmake/toolchains/linux-gnu-x86_64.cmake
+++ cmake/toolchains/linux-gnu-x86_64.cmake
@@ -6,14 +6,23 @@ set(CMAKE_SYSTEM_PROCESSOR x86_64)
 set(CMAKE_C_COMPILER gcc)
 set(CMAKE_Fortran_COMPILER gfortran)
 
-# Flags
+# Base flags - these will be the default if no other flags are specified
 set(CMAKE_C_FLAGS_INIT "-march=x86-64")
 set(CMAKE_Fortran_FLAGS_INIT "-march=x86-64")
 
-# Release flags
+# You can also set flags directly, but this might override user preferences
+# set(CMAKE_C_FLAGS "-march=x86-64")
+# set(CMAKE_Fortran_FLAGS "-march=x86-64")
+
+# Build-type specific flags
 set(CMAKE_C_FLAGS_RELEASE_INIT "-O3")
 set(CMAKE_Fortran_FLAGS_RELEASE_INIT "-O3")
 
-# Debug flags
 set(CMAKE_C_FLAGS_DEBUG_INIT "-g -O0")
 set(CMAKE_Fortran_FLAGS_DEBUG_INIT "-g -O0 -fcheck=all -fbacktrace")
+
+# Example of how flags combine:
+# Final Debug flags will be:
+#   ${CMAKE_C_FLAGS_INIT} + user's CMAKE_C_FLAGS + ${CMAKE_C_FLAGS_DEBUG_INIT} + user's CMAKE_C_FLAGS_DEBUG
+# Final Release flags will be:
+#   ${CMAKE_C_FLAGS_INIT} + user's CMAKE_C_FLAGS + ${CMAKE_C_FLAGS_RELEASE_INIT} + user's CMAKE_C_FLAGS_RELEASE
